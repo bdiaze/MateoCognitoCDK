6b759e40efd2d71dc79c321007cae1a55c5e9993
Benjamín <benjamin.diaze@outlook.com>
2025-01-31 19:12:35 -0300

    Se crea UserPool

--- CDK/src/Cdk/CdkStack.cs
+++ CDK/src/Cdk/CdkStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Cognito;
 using Constructs;
 
 namespace Cdk
@@ -7,7 +8,43 @@ namespace Cdk
     {
         internal CdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+
+            UserPool userPool = new UserPool(this, "MateoUserPool", new UserPoolProps {
+                UserPoolName = "MateoUserPool",
+                SelfSignUpEnabled = true,
+                SignInAliases = new SignInAliases {
+                    Email = true
+                },
+                AutoVerify = new AutoVerifiedAttrs {
+                    Email = true
+                },
+                StandardAttributes = new StandardAttributes {
+                    Email = new StandardAttribute {
+                        Required = true,
+                        Mutable = false
+                    },
+                    Nickname = new StandardAttribute {
+                        Required = true,
+                        Mutable = true
+                    },
+                    Birthdate = new StandardAttribute {
+                        Required = true,
+                        Mutable = true
+                    },
+                    Gender = new StandardAttribute {
+                        Required = true,
+                        Mutable = true
+                    },
+                },
+                PasswordPolicy = new PasswordPolicy {
+                    MinLength = 8,
+                    RequireLowercase = true,
+                    RequireUppercase = true,
+                    RequireDigits = true,
+                    RequireSymbols = true
+                }
+            });
+
         }
     }
 }
