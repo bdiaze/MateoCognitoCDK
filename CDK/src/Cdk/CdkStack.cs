dd49509af107adb60f5fca7833229d64cd511bc4
Benjamín <benjamin.diaze@outlook.com>
2025-01-31 20:23:42 -0300

    Se añade UserPoolClient

--- CDK/src/Cdk/CdkStack.cs
+++ CDK/src/Cdk/CdkStack.cs
@@ -12,16 +12,30 @@ namespace Cdk
             UserPool userPool = new UserPool(this, "MateoUserPool", new UserPoolProps {
                 UserPoolName = "MateoUserPool",
                 SelfSignUpEnabled = true,
+                UserVerification = new UserVerificationConfig {
+                    EmailSubject = "¡Verifica tu correo electrónico! - Mateo",
+                    EmailBody = "¡Hola {username}!, Para verificar tu correo electrónico solo debes hacer click {##¡Aquí!##}",
+                    EmailStyle = VerificationEmailStyle.LINK,
+                },
                 SignInAliases = new SignInAliases {
+                    Username = true,
                     Email = true
                 },
                 AutoVerify = new AutoVerifiedAttrs {
                     Email = true
                 },
+                KeepOriginal = new KeepOriginalAttrs { 
+                    Email = true
+                },
+                Mfa = Mfa.OPTIONAL,
+                MfaSecondFactor = new MfaSecondFactor {
+                    Otp = true,
+                },
+                AccountRecovery = AccountRecovery.EMAIL_ONLY,
                 StandardAttributes = new StandardAttributes {
                     Email = new StandardAttribute {
                         Required = true,
-                        Mutable = false
+                        Mutable = true
                     },
                     Nickname = new StandardAttribute {
                         Required = true,
@@ -45,6 +59,40 @@ namespace Cdk
                 }
             });
 
+            UserPoolClient userPoolClient = new UserPoolClient(this, "MateoUserPoolClient", new UserPoolClientProps {
+                UserPool = userPool,
+                GenerateSecret = false,
+                PreventUserExistenceErrors = true,
+                ReadAttributes = new ClientAttributes().WithStandardAttributes(new StandardAttributesMask { 
+                    Email = true,
+                    Nickname = true,
+                    Birthdate = true,
+                    Gender = true
+                }),
+                SupportedIdentityProviders = new UserPoolClientIdentityProvider[] {
+                    UserPoolClientIdentityProvider.COGNITO
+                },
+                AuthFlows = new AuthFlow {
+                    UserPassword = true
+                },
+                OAuth = new OAuthSettings { 
+                    Flows = new OAuthFlows {
+                        AuthorizationCodeGrant = true
+                    },
+                    Scopes = new OAuthScope[] {
+                        OAuthScope.EMAIL,
+                        OAuthScope.OPENID,
+                        OAuthScope.PROFILE,
+                        OAuthScope.COGNITO_ADMIN
+                    },
+                    CallbackUrls = new string[] {
+                        "http://localhost/signin-oidc"
+                    },
+                    LogoutUrls = new string[] {
+                        "http://localhost/logout-oidc"
+                    }
+                }
+            });
         }
     }
 }
