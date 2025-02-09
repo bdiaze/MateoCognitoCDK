46212f4438228afa9c7905116231ad92c5f00608
Benjamín <benjamin.diaze@outlook.com>
2025-02-09 20:32:25 -0300

    Se deja nuevo client con clientcredentials

--- src/Cdk/CdkStack.cs
+++ src/Cdk/CdkStack.cs
@@ -98,8 +98,6 @@ namespace Cdk
                 },
                 OAuth = new OAuthSettings {
                     Flows = new OAuthFlows {
-                        AuthorizationCodeGrant = true,
-                        ImplicitCodeGrant = true,
                         ClientCredentials = true,
                     },
                     Scopes = new[] {
